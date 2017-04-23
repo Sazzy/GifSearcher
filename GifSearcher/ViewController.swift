0dce48868ba202ec0fd23280c7c4e201fd9d0064
Eugene Sazonov <saz.vit@gmail.com>
2017-04-23 12:45:15 -0700

    Extended with delegates for tableview

--- GifSearcher/ViewController.swift
+++ GifSearcher/ViewController.swift
@@ -1,13 +1,16 @@
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var gifTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchTextField.delegate = self
         searchTextField.returnKeyType = UIReturnKeyType.search
-        // Do any additional setup after loading the view, typically from a nib.
+        gifTableView.estimatedRowHeight = 70.0
+        gifTableView.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
@@ -16,7 +19,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
+        //gifTableView.reloadData()
         return true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 
 }
